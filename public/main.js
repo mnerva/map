3fe5eaca13b59fec00fc777b7066ac8ec502fbd1
mnerva <mona-rea.nerva@voco.ee>
2025-05-15 11:30:11 +0300

    added map style control button

--- public/main.js
+++ public/main.js
@@ -10,6 +10,7 @@ window.addEventListener('DOMContentLoaded', async () => {
     const citiesBtn = document.getElementById('citiesBtn');
     const foodBtn = document.getElementById('foodBtn');
     const booksBtn = document.getElementById('booksBtn');
+    const mapStyle = document.querySelector('.mapstyles-select')
 
     if (citiesBtn) {
       citiesBtn.addEventListener('click', async () => {
@@ -41,6 +42,15 @@ window.addEventListener('DOMContentLoaded', async () => {
         } catch (err) {
           console.error('Failed to load cities:', err);
         }
+      });
+    }
+
+    if (mapStyle) {
+      mapStyle.addEventListener('change', (e) => {
+        const style_code = e.target.value.split(".");
+        style_code.length === 2 ?
+          map.setStyle(maptilersdk.MapStyle[style_code[0]][style_code[1]]) :
+          map.setStyle(maptilersdk.MapStyle[style_code[0]] || style_code[0]);
       });
     }
   } catch (err) {
