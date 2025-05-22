2d5f19041b25db0ca05ad26f3dfc7fd352f6fb0e
mnerva <mona-rea.nerva@voco.ee>
2025-05-22 09:11:19 +0300

    added one console log

--- server/controllers/maptilerController.js
+++ server/controllers/maptilerController.js
@@ -1,4 +1,5 @@
 export const getMapTilerKey = (req, res) => {
   console.log("MapTiler controller loaded");
+  console.log("API Key:", process.env.MAPTILER_API_KEY);
   res.json({ apiKey: process.env.MAPTILER_API_KEY });
 };
