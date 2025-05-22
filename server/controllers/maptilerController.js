0419a0b0f1c5e9c1433d21b4dd268e2f7766e451
mnerva <mona-rea.nerva@voco.ee>
2025-05-22 12:56:56 +0300

    changed it back

--- server/controllers/maptilerController.js
+++ server/controllers/maptilerController.js
@@ -1,7 +1,4 @@
 export const getMapTilerKey = (req, res) => {
-  if (req.headers.authorization !== process.env.SECRET_TOKEN) {
-    return res.status(403).send('Forbidden');
-  }
   const key = process.env.MAPTILER_API_KEY;
   res.json({ apiKey: key });
 };
