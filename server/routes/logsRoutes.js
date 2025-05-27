6f5cb314719ceb43212410082e3eedbe9bd52a53
mnerva <mona-rea.nerva@voco.ee>
2025-05-27 15:54:51 +0300

    fix again

--- server/routes/logsRoutes.js
+++ server/routes/logsRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { downloadLogs } from '../controllers/logsController.js';
-import { basicAuthMiddleware } from '../middleware/basicAuthMiddleware.js';
+import basicAuthMiddleware from '../middleware/basicAuthMiddleware.js';
 
 const router = express.Router();
 
